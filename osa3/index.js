46afdb084d7d79f656c150df205285e83da354d1
okkupokku <okarturkkilainen@gmail.com>
2024-05-21 19:37:43 +0300

    ex 3.4 tehty

--- osa3/index.js
+++ osa3/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
@@ -31,6 +31,13 @@ app.get('/info', (req, res) => {
     } else {
       res.status(404).send({ error: 'Person not found' });
     }
+  });
+
+  app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+  
+    res.status(204).end();
   });
 
 const PORT = 3001;
